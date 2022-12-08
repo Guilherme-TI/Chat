de59bb5574de179479bc0f6c4d7d3d9743dc733f
Luiz Guilherme <90113778+Guilherme-TI@users.noreply.github.com>
2022-12-08 18:31:13 -0300

    Update view_nav.php

--- view_nav.php
+++ view_nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
     <div class="container">
-        <a class="navbar-brand" href="view_home.php">Aggregate</a>
+        <a class="navbar-brand" href="view_home.php">Chat</a>
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="view_home.php"><img src="img/casa.png" width="35em"></a>
